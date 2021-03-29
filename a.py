ff287dab6c093d5ce3275e779fc8553bdd760f7a
Abhiney27 <63646182+Abhiney27@users.noreply.github.com>
2021-03-29 21:29:49 +0530

    .

--- a.py
+++
@@ -1,9 +0,0 @@
-import joblib
-model = joblib.load('housemodel.pk1')
-income = 80000
-house_age = 3
-room = 8
-bed_room = 4
-population  =23086.800503
-m = model.predict([[income , house_age , room , bed_room , population]])
-print(str(m[0]))
